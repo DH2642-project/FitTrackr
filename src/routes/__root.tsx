6aaa846c1ffd510c8f851b85c51fb06a986afc11
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2024-03-20 23:47:46 +0000

    Remove unused code

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,29 +1,31 @@
+import { Container } from "@mui/material"
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router"
 import React, { Suspense } from "react"
 
+// TanStack devtools only in development
 const TanStackRouterDevtools =
   process.env.NODE_ENV === "production"
     ? () => null // Render nothing in production
     : React.lazy(() =>
-        // Lazy load in development
-        import("@tanstack/router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-          // For Embedded Mode
-          // default: res.TanStackRouterDevtoolsPanel
-        }))
-      )
+      // Lazy load in development
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      }))
+    )
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
+      <Container>
+        <Link to="/">
           Home
         </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
+        <Link to="/about">
           About
         </Link>
-      </div>
+      </Container>
       <hr />
       <Outlet />
 
