c1ac1ae120335df4ae86cda6f9b328449eecef7c
Elias Floreteng <18127101+elias123tre@users.noreply.github.com>
2024-03-21 00:12:58 +0000

    Init firebase services

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import React, { StrictMode } from "react"
+import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
 import { ThemeProvider } from "@emotion/react"
 import { CssBaseline } from "@mui/material"
 import theme from "./theme"
-
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen"
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth"
+import { routeTree } from "./routeTree.gen" // Import the generated route tree
 
 // Create a new router instance
 const router = createRouter({ routeTree })
@@ -18,13 +19,8 @@ declare module "@tanstack/react-router" {
   }
 }
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCEoStnp59JE3vmVMc_Y0K8cNw-A7haUSQ",
   authDomain: "dh2642-project-7eb8e.firebaseapp.com",
   databaseURL: "https://dh2642-project-7eb8e-default-rtdb.europe-west1.firebasedatabase.app",
@@ -35,7 +31,10 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+
+export const database = getDatabase(app);
+export const auth = getAuth(app);
 
 // Render the app
 const rootElement = document.getElementById("root")!
