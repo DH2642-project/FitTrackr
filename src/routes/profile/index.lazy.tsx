cfbbcdc2598cfc15b18dfa7eef32564054b45ecc
Linus Dinesjö <linus.dinesjo@gmail.com>
2024-03-25 15:55:06 +0100

    Add snackbar and Email sign-in

--- src/routes/profile/index.lazy.tsx
+++ src/routes/profile/index.lazy.tsx
@@ -4,7 +4,7 @@ import LoginForm from "../../components/LoginForm";
 import LoggedIn from "../../components/LoggedIn";
 import { auth } from "../../main";
 import { useEffect, useState } from "react";
-import { CircularProgress } from "@mui/material";
+import { Alert, AlertColor, CircularProgress, Snackbar } from "@mui/material";
 import firebase from "firebase/compat/app";
 import { onAuthStateChanged } from "firebase/auth";
 
@@ -12,14 +12,27 @@ function Profile() {
   const [loading, setLoading] = useState(true);
   // TODO: user should be in model or store?
   const [user, setUser] = useState<firebase.User | null>(firebase.auth().currentUser);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarSeverity, setSnackbarSeverity] = useState<AlertColor>();
+
+  function showSnackbar(message: string, severity: AlertColor = "info") {
+    setSnackbarMessage(message);
+    setSnackbarOpen(true);
+    setSnackbarSeverity(severity);
+  }
 
   function handleLogout() {
     firebase
       .auth()
       .signOut()
+      .then(() => {
+        // Sign-out successful.
+        showSnackbar("Successfully signed out!", "success");
+      })
       .catch((error) => {
         // Handle error
-        console.error("Error signing out:", error);
+        showSnackbar(`Error signing out: ${error.message}`, "error");
       });
   }
 
@@ -41,7 +54,21 @@ function Profile() {
     );
   }
 
-  return <Cover>{user ? <LoggedIn user={user} logout={handleLogout} /> : <LoginForm />}</Cover>;
+  return (
+    <Cover>
+      {user ? <LoggedIn user={user} logout={handleLogout} /> : <LoginForm />}
+      <Snackbar
+        open={snackbarOpen}
+        message={snackbarMessage}
+        autoHideDuration={5000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <Alert onClose={() => setSnackbarOpen(false)} severity={snackbarSeverity}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </Cover>
+  );
 }
 
 export const Route = createLazyFileRoute("/profile/")({
