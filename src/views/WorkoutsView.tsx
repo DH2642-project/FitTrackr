c025d8821dde2d84978271e64f39c71694e5c660
Gustaf Svensson <gussvens@kth.se>
2024-04-04 15:55:43 +0200

    The workouts render in different components based on date of workout

--- src/views/WorkoutsView.tsx
+++ src/views/WorkoutsView.tsx
@@ -34,30 +34,71 @@ export default function WorkoutsView({
     return 0;
   });
 
+  const now = new Date();
+  now.setHours(0, 0, 0, 0);
+  const tomorrow = new Date(now);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  const previousWorkouts = sortedWorkouts.filter(workout => new Date(workout.date as string) < now);
+  const todaysWorkouts = sortedWorkouts.filter(workout => new Date(workout.date as string) >= now && new Date(workout.date as string) < tomorrow);
+  const upcomingWorkouts = sortedWorkouts.filter(workout => new Date(workout.date as string) >= tomorrow);
+
   return (
-    <>
-      <Container sx={{ p: { xs: 1, md: 2 }, width: "100%", height: "100%" }}>
-        <Typography variant="h4" align="center" gutterBottom>
-          My Workouts
-        </Typography>
-        {workoutsLoading && <FullscreenCircularProgress />}
-        <Typography variant="h5" align="left" gutterBottom>
-          Upcoming Workouts
-        </Typography>
-        <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-          {!workoutsLoading && sortedWorkouts.length > 0 ? (
-            sortedWorkouts.map((workout) => (
-              <WorkoutsCard key={workout.key} workout={workout} deleteWorkout={deleteWorkout} />
-            ))
-          ) : (
-            <Grid item xs={12}>
-              <Typography variant="h5" align="center">
-                No workouts found
-              </Typography>
-            </Grid>
-          )}
-        </Grid>
-      </Container>
-    </>
+    <Container sx={{ p: { xs: 1, md: 2 }, width: "100%", height: "100%" }}>
+      <Typography variant="h4" align="center" gutterBottom>
+        My Workouts
+      </Typography>
+      {workoutsLoading && <FullscreenCircularProgress />}
+      <Typography variant="h5" align="left" gutterBottom>
+        Today's Workouts
+      </Typography>
+      <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {!workoutsLoading && todaysWorkouts.length > 0 ? (
+          todaysWorkouts.map((workout) => (
+            <WorkoutsCard key={workout.key} workout={workout} deleteWorkout={deleteWorkout} />
+          ))
+        ) : (
+          <Grid item xs={12}>
+            <Typography variant="h6" align="left">
+              There are no workout scheduled for today
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
+      
+      <Typography variant="h5" align="left" gutterBottom>
+        Upcoming Workouts
+      </Typography>
+      <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {!workoutsLoading && upcomingWorkouts.length > 0 ? (
+          upcomingWorkouts.map((workout) => (
+            <WorkoutsCard key={workout.key} workout={workout} deleteWorkout={deleteWorkout} />
+          ))
+        ) : (
+          <Grid item xs={12}>
+            <Typography variant="h6" align="left">
+              There are no upcoming workouts
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
+
+      <Typography variant="h5" align="left" gutterBottom>
+        Previous Workouts
+      </Typography>
+      <Grid container spacing={{ xs: 1, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {!workoutsLoading && previousWorkouts.length > 0 ? (
+          previousWorkouts.map((workout) => (
+            <WorkoutsCard key={workout.key} workout={workout} deleteWorkout={deleteWorkout} />
+          ))
+        ) : (
+          <Grid item xs={12}>
+            <Typography variant="h6" align="left">
+              There are no previous workouts
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
+    </Container>
   );
 }
